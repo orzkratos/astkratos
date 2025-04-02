ca6c39ed19f71c3334850de95f81edc407a5690f
yangyile <gogit@github.com/go-xlan/gogit>
2025-04-02 16:58:48 +0700

    把demo相关的东西独立到新项目里

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # astkratos
 Kratos project code analysis with Go AST.
+
+## demos
+please see demo project: [astkratos-demos](https://github.com/orzkratos/astkratos-demos)
--- astkratos_test.go
+++ astkratos_test.go
@@ -1,51 +1,9 @@
 package astkratos_test
 
 import (
-	"path/filepath"
 	"testing"
-
-	"github.com/orzkratos/astkratos"
-	"github.com/orzkratos/astkratos/internal/demos"
-	"github.com/yyle88/neatjson/neatjsons"
-	"github.com/yyle88/zaplog"
 )
 
-var projectPath string
-
 func TestMain(m *testing.M) {
-	projectPath = demos.CreateDemoProjectWhenNotExist()
-
-	zaplog.SUG.Debugln(projectPath)
 	m.Run()
-}
-
-func TestListGrpcClients(t *testing.T) {
-	definitions := astkratos.ListGrpcClients(filepath.Join(projectPath, "api"))
-	t.Log(neatjsons.S(definitions))
-}
-
-func TestListGrpcServers(t *testing.T) {
-	definitions := astkratos.ListGrpcServers(filepath.Join(projectPath, "api"))
-	t.Log(neatjsons.S(definitions))
-}
-
-func TestListGrpcServices(t *testing.T) {
-	definitions := astkratos.ListGrpcServices(filepath.Join(projectPath, "api"))
-	t.Log(neatjsons.S(definitions))
-}
-
-func TestListGrpcUnimplementedServers(t *testing.T) {
-	definitions := astkratos.ListGrpcUnimplementedServers(filepath.Join(projectPath, "api"))
-	t.Log(neatjsons.S(definitions))
-}
-
-func TestListStructsMap(t *testing.T) {
-	structsMap := astkratos.ListStructsMap(filepath.Join(projectPath, "api/helloworld/v1/greeter.pb.go"))
-	t.Log(len(structsMap))
-
-	for name, definition := range structsMap {
-		t.Log(name)
-
-		t.Log(definition.Name, definition.StructCode)
-	}
 }
